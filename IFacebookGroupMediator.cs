1ec3045cf6987fdd191b72ccfdd4ba4f2facd8d7
kananbayramli <bayramli-kenan@mail.ru>
2023-05-22 21:22:53 +0400

    design pattern mediator

---
+++ IFacebookGroupMediator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorDesignPattern
+{
+    public interface IFacebookGroupMediator
+    {
+        //This Method is used to send the Message who are registered with the Facebook Group
+        void SendMessage(string msg, User user);
+        //This method is used to register a user with the Facebook Group
+        void RegisterUser(User user);
+    }
+}
---
+++ User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorDesignPattern
+{
+    public abstract class User
+    {
+        //This Property holds the name of the user
+        protected string Name;
+        //This Property is going to set and get the Mediator Instance
+        //This Property value is going to be set when we register a user with the Mediator
+        public IFacebookGroupMediator Mediator { get; set; }
+
+        //Initializing the name using Constructor
+        public User(string name)
+        {
+            this.Name = name;
+        }
+        //The following Methods are going to be Implemented by the Concrete Colleague
+        public abstract void Send(string message);
+        public abstract void Receive(string message);
+    }
+}
